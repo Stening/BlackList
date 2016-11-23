226810d8ae16270187b751594ac202295d8b18df
Johan Stening <johan.stening@hotmail.com>
2016-11-23 13:15:22 +0100

    Created Database

---
+++ BlackList/BlackList/Models/ShoppingList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BlackList.Models
+{
+    public class ShoppingList
+    {
+        [Key, Required]
+        public int ShoppingListID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+
+
+
+    }
+}
