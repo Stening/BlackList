cf874b5ada88b084c55531865938a8fc8c251bbe
Johan Stening <johan.stening@hotmail.com>
2016-11-30 15:32:09 +0100

    fixed some seed errors

--- BlackList/BlackList/Migrations/Configuration.cs
+++ BlackList/BlackList/Migrations/Configuration.cs
@@ -7,15 +7,15 @@ namespace BlackList.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<BlackList.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<BlackList.Models.BlackListRepository>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "BlackList.Models.ApplicationDbContext";
+            //ContextKey = "BlackList.Models.ApplicationDbContext";
         }
 
-        protected override void Seed(BlackList.Models.ApplicationDbContext context)
+        protected override void Seed(BlackList.Models.BlackListRepository context)
         {
             User Dummy = new User { IsMailVerified = false, PhoneNumber = 123, UserName = "dummy", DateCreated = DateTime.Now, Mail = "test@testmail.com" };
             User Dummy2 = new User { IsMailVerified = false, PhoneNumber = 123, UserName = "dummy2", DateCreated = DateTime.Now, Mail = "test2@testmail.com" };
@@ -33,59 +33,59 @@ namespace BlackList.Migrations
 
 
 
-            
 
 
 
 
 
-            context.Contacts.AddOrUpdate(p => p.Name,
-               new Contact
-               {
-                   Name = "Debra Garcia",
-                   Address = "1234 Main St",
-                   City = "Redmond",
-                   State = "WA",
-                   Zip = "10999",
-                   Email = "debra@example.com",
-               },
-                new Contact
-                {
-                    Name = "Thorsten Weinrich",
-                    Address = "5678 1st Ave W",
-                    City = "Redmond",
-                    State = "WA",
-                    Zip = "10999",
-                    Email = "thorsten@example.com",
-                },
-                new Contact
-                {
-                    Name = "Yuhong Li",
-                    Address = "9012 State st",
-                    City = "Redmond",
-                    State = "WA",
-                    Zip = "10999",
-                    Email = "yuhong@example.com",
-                },
-                new Contact
-                {
-                    Name = "Jon Orton",
-                    Address = "3456 Maple St",
-                    City = "Redmond",
-                    State = "WA",
-                    Zip = "10999",
-                    Email = "jon@example.com",
-                },
-                new Contact
-                {
-                    Name = "Diliana Alexieva-Bosseva",
-                    Address = "7890 2nd Ave E",
-                    City = "Redmond",
-                    State = "WA",
-                    Zip = "10999",
-                    Email = "diliana@example.com",
-                }
-                );
+
+            //Wcontext.Contacts.AddOrUpdate(p => p.Name,
+            //   new Contact
+            //   {
+            //       Name = "Debra Garcia",
+            //       Address = "1234 Main St",
+            //       City = "Redmond",
+            //       State = "WA",
+            //       Zip = "10999",
+            //       Email = "debra@example.com",
+            //   },
+            //    new Contact
+            //    {
+            //        Name = "Thorsten Weinrich",
+            //        Address = "5678 1st Ave W",
+            //        City = "Redmond",
+            //        State = "WA",
+            //        Zip = "10999",
+            //        Email = "thorsten@example.com",
+            //    },
+            //    new Contact
+            //    {
+            //        Name = "Yuhong Li",
+            //        Address = "9012 State st",
+            //        City = "Redmond",
+            //        State = "WA",
+            //        Zip = "10999",
+            //        Email = "yuhong@example.com",
+            //    },
+            //    new Contact
+            //    {
+            //        Name = "Jon Orton",
+            //        Address = "3456 Maple St",
+            //        City = "Redmond",
+            //        State = "WA",
+            //        Zip = "10999",
+            //        Email = "jon@example.com",
+            //    },
+            //    new Contact
+            //    {
+            //        Name = "Diliana Alexieva-Bosseva",
+            //        Address = "7890 2nd Ave E",
+            //        City = "Redmond",
+            //        State = "WA",
+            //        Zip = "10999",
+            //        Email = "diliana@example.com",
+            //    }
+            //    );
             base.Seed(context);
 
 
@@ -105,4 +105,5 @@ namespace BlackList.Migrations
 
 
         }
+    }
 }
