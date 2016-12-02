a748bb6ef1bd2c0e5a17051f1b8eb3d99303459c
johan stening <johan.stening@hotmail.com>
2016-12-02 13:24:48 +0100

    going to run a test on aspnetUser

---
+++ BlackList/BlackList/Models/ChatRoomUsers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BlackList.Models
+{
+    public class ChatRoomUsers
+    {
+
+        public int ChatRoomID { get; set; }
+
+        [ForeignKey("user"), Key, Column(Order = 0)]
+        public int UserID { get; set; }
+        public User user{ get; set; }
+
+    }
+}
