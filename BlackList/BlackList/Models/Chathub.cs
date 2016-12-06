ba2ffd87673fbe4e0d550b81c87c2edccb245a8e
Erik Petersson <erik__petersson@hotmail.com>
2016-12-06 12:34:04 +0100

    Erik5.0 added chat

---
+++ BlackList/BlackList/Models/Chathub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlackList.Models
+{
+    public class ChatHub : Hub
+    {
+        // Authorize the method, user logged in will only be able to use this feature.
+        // The method will be called from the ChatAll.js file, containing 2 string variables.
+        //[Authorize]
+        public void Send(string name, string message)
+        {
+            // Sends this method back to the function in ChatAll.js that creates <li>name : message</li>.
+            // For each message sent.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
