0c2cf9bb63a0dc2cc2c8711dd2ed2a533bc809fd
Niclas Liinanki <trolldans@gmail.com>
2016-12-07 15:15:14 +0100

    update of JS read list

--- BlackList/BlackList/Scripts/custom/ReadingOfTheList.js
+++ BlackList/BlackList/Scripts/custom/ReadingOfTheList.js
@@ -1,15 +1,30 @@
-﻿
-$(function () {
-    var arr = ["one", "two", "three", "four"];
-    var obj = { one: 1, two: 2, three: 3, four: 4};
+﻿/*====================================
+            On start
+====================================*/
+$(document).ready(function () {
+    var CL = $.connection.cRUDHub;
 
-    $.each(arr, function (i, val) {
-        $("#" + val).attr(id,  val);
+    /*====================================
+            Reading of the list
+    ====================================*/
+    $.connection.hub.start().done(function () {
+        $('#read-list-button').click(function () {
+            CL.server.readTheListJS($('#textbox-list').val(), $('.headingForListName').prop('id'));
+
+
+
+        });
     });
-
-    $.each(obj, function (i, val) {
-        $("#" + i).append(document.createTextNode(" - " + val));
+    /*====================================
+       Generating list item from idlist
+    ====================================*/
+    $(function () {
+        var arr = ["101", "102", "103", "104"];
+        $.each(arr, function (i, val) {
+            $("#" + val).text("ID: " + val);
+        });
+        $.each(arr, function (i, val) {
+            $("#" + i).append(document.createTextNode(", " + val));
+        });
     });
-
-
 });
