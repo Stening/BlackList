13779c2fb572c7068b2af0e023d8a3ee1cb57d29
Niclas Liinanki <trolldans@gmail.com>
2016-12-08 08:48:53 +0100

    small changes to JS

--- BlackList/BlackList/Scripts/custom/ReadingOfTheList.js
+++ BlackList/BlackList/Scripts/custom/ReadingOfTheList.js
@@ -8,23 +8,38 @@ $(document).ready(function () {
             Reading of the list
     ====================================*/
     $.connection.hub.start().done(function () {
-        $('#read-list-button').click(function () {
-            CL.server.readTheListJS($('#textbox-list').val(), $('.headingForListName').prop('id'));
-
-
-
+        $('#add-to-list-button').click(function () {
+            CL.server.addToListCode($('#textbox-list').val(), $('.headingForListName').prop('id'));
+            
+            //readTheListJS
         });
     });
-    /*====================================
-       Generating list item from idlist
-    ====================================*/
-    $(function () {
-        var arr = ["101", "102", "103", "104"];
-        $.each(arr, function (i, val) {
-            $("#" + val).text("ID: " + val);
-        });
-        $.each(arr, function (i, val) {
-            $("#" + i).append(document.createTextNode(", " + val));
+
+    CL.client.addToList = function (wordsInList, id) {
+        var cList = $('<ul/>')
+        .addClass('ul-ShoppingList');
+
+        var li = $('<li/>')
+        .addClass('li-ShoppingList')
+        .prop("id", id)
+        .appendTo(cList);
+
+        var text = $('<p/>')
+        .addClass('word-in-p')
+        .text(wordsInList)
+        .appendTo(li);
+    };
+
+        /*====================================
+           Generating list item from idlist
+        ====================================*/
+        $(function () {
+            var arr = ["101", "102", "103", "104"];
+            $.each(arr, function (i, val) {
+                $("#" + val).text("ID: " + val);
+            });
+            $.each(arr, function (i, val) {
+                $("#" + i).append(document.createTextNode(", " + val));
+            });
         });
     });
-});
