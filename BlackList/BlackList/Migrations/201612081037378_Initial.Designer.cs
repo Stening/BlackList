1170a6bc7673d8bb7c1fdd6c64d34e488c814302
Johan Stening <johan.stening@hotmail.com>
2016-12-08 11:40:25 +0100

    ONLY THIS MIGRATION FILE IS ALLOWED TO BE RUN

--- BlackList/BlackList/Migrations/201612081037378_Initial.Designer.cs
+++ BlackList/BlackList/Migrations/201612081037378_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace BlackList.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class DboUsers : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(DboUsers));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201612050731313_DboUsers"; }
+            get { return "201612081037378_Initial"; }
         }
         
         string IMigrationMetadata.Source
