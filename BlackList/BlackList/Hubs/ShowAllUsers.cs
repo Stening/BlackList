8682935dc98c181a4ebdfe5adbb310c9a68a79fb
Wigge <wigge_94@live.se>
2016-12-08 13:42:49 +0100

    Pre-merge with production

---
+++ BlackList/BlackList/Hubs/ShowAllUsers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using BlackList.Models;
+using System.Collections;
+
+namespace BlackList.Hubs
+{
+    public class ShowAllUsers : Hub
+    {
+
+        public ApplicationDbContext _context = new ApplicationDbContext();
+
+        public ShowAllUsers()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public void Send()
+        {
+            var allTheUsers = getAllUsers();
+            Clients.Caller.updateUserList(allTheUsers);
+
+        }
+
+        // Have to make the method only take the people that are non-friends.
+        public IEnumerable getAllUsers()
+        {
+            var users = from user
+                        in _context.Users
+                        select user.UserName;
+
+            return users.ToArray();
+        }
+
+
+    }
+}
