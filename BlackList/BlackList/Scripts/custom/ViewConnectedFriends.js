6d5e6799ecd2fb9d8c9d8db015db90096e765b77
johan stening <johan.stening@hotmail.com>
2016-12-10 15:48:27 +0100

    added glyph icons

--- BlackList/BlackList/Scripts/custom/ViewConnectedFriends.js
+++ BlackList/BlackList/Scripts/custom/ViewConnectedFriends.js
@@ -6,11 +6,11 @@
     $.connection.hub.start().done(function () {
 
 
-        
+
 
         FriendsConnection.server.connectToFriends();
 
-       
+
 
 
 
@@ -19,26 +19,38 @@
     FriendsConnection.client.renderFriends = function (friends) {
         FriendList = friends;
         $("#Friends").empty();
-
-        $("#Friends").append("<ul>")
         for (var i = 0; i < friends.length; i++) {
+            $("#InviteFriends").append("<div>" + friends[i].UserName + "<button class='AddFriendToList'>Add to list</button>" + "</div>");
+            var toAppend = "<div class='col-md-8'>" + friends[i].UserName;
 
-            $("#Friends").append("<li>" + friends[i].UserName + " " + friends[i].Online + "</li>");
-            $("#InviteFriends").append("<li>" + friends[i].UserName + "<button class='AddFriendToList'>Add to list</button>"+"</li>");
 
+            if (friends[i].Online == true) {
+
+                toAppend += " <span class='glyphicon glyphicon-ok'>";
+            }
+            else {
+                toAppend += " <span class='glyphicon glyphicon-remove'>";
+            }
+            toAppend += "</div>";
+            $("#Friends").append(toAppend)
         }
-        $("#Friends").append("</ul>")
 
 
 
 
     }
 
+
+
+
+
+
+
     $('body').on('click', '.AddFriendToList', function () {
         console.log("Here We will invite the friend to the list, however first we need tha ability to add multiple lists to be able to invite to that specific list");
         //console.log($(this).parent());
     });
-    
+
 
 
 
@@ -50,5 +62,5 @@
 
     }
 
-   
+
 })
