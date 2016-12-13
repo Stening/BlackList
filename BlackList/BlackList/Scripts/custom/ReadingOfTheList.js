6ef0cde9fb69314b939cd0aa1bf7541e1b00e1a3
Josefine Rutgerson <josefinerutgerson@gmail.com>
2016-12-13 10:35:19 +0100

    Update production branch Josefine

--- BlackList/BlackList/Scripts/custom/ReadingOfTheList.js
+++ BlackList/BlackList/Scripts/custom/ReadingOfTheList.js
@@ -30,11 +30,7 @@ $(document).ready(function () {
                 .appendTo(liInMeny)
 
            
-            $('#myLists').append(ulListInMeny);
-
-
-
-   
+            $('#myLists').append(ulListInMeny); 
 
         
 
@@ -46,35 +42,19 @@ $(document).ready(function () {
     /*====================================
        Generating list item from idlist
     ====================================*/
-  
-        var arr = ["101", "102", "103", "104"];
-
-
-
-
-
 
     var CrudConnection = $.connection.cRUDHub;
     $.connection.hub.start().done(function () {
-
-
         CrudConnection.server.getMyLists();
-
         
     })
 
+
     $('body').on('click', '#MyLists', function () {
-
-
         CrudConnection.server.getListItems();
-
-
     })
 
-
-
     CrudConnection.client.renderMyLists = function (myLists) {
-
         var html = "<ul>";
 
         for (var i = 0; i < myLists.length; i++) {
@@ -83,7 +63,83 @@ $(document).ready(function () {
         html += "</ul>";
         $("#MyLists").append(html);
         console.log(myLists);
+        
+    }
 
+
+    function readListFromMenu(myLists) {
+
+        //Skapar listan med ord
+        //var cList = $('<ul/>')
+        //.addClass('ul-ShoppingList');
+        
+        $.each(myLists[i], function (i) {
+
+            var li = $('<li/>')
+                .addClass('li-ShoppingList')
+                .addClass('click-sList')
+                .prop("id", myList[i].ListItemID);
+
+            var defaultDiv = $('<div />')
+                .addClass('default-div-word')
+                .appendTo(li);
+
+            var text = $('<p/>')
+                .addClass('word-in-p')
+                .addClass('col-lg-5')
+                .text(myLists[i].ItemName)
+                .click(toggleListWords)
+                .appendTo(defaultDiv);
+
+            var spanInList = $('<span/>')
+                    .addClass('glyphicon')
+                    .addClass('glyphicon-ok')
+                    .addClass('bock-class')
+                    .appendTo(text);
+
+            var trashButtonInList = $('<button />')
+                .addClass('remove-button-class')
+                .click(deleteWord)
+                .appendTo(defaultDiv);
+
+            var trashGlyphInButton = $('<span/>')
+                .addClass('glyphicon')
+                .addClass('glyphicon-trash')
+                .appendTo(trashButtonInList);
+
+            var editButtonInList = $('<button />')
+                .addClass('edit-button-class')
+                .click(editWord)
+                .appendTo(defaultDiv);
+
+            var editGlyphInButton = $('<span />')
+                .addClass('glyphicon')
+                .addClass('glyphicon-pencil')
+                .appendTo(editButtonInList);
+
+            var updateDiv = $('<div />')
+            .addClass('update-word-div')
+            .appendTo(li);
+
+            var newTextBox = $('<input>', {
+                type: 'text',
+                val: wordsInList
+            }).addClass('textbox-for-update').prop('id', 'editBox' + id).appendTo(updateDiv);
+
+            var editButton = $('<button />')
+                .addClass('save-new-word')
+                .click(sendNewWord)
+                .appendTo(updateDiv);
+
+            var updateGlyphInButton = $('<span />')
+                .addClass('glyphicon')
+                .addClass('glyphicon-floppy-saved')
+                .appendTo(editButton);
+
+
+            $('#theUlList').append(li);
+
+        });
     }
 
   
