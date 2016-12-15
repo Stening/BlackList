0836a47e691c82a0e40bfcfcf2e9a8a9cc189af2
Wigge <wigge_94@live.se>
2016-12-15 10:38:27 +0100

    Generate some html to the view of a list with users

--- BlackList/BlackList/Scripts/custom/GetAllUsers.js
+++ BlackList/BlackList/Scripts/custom/GetAllUsers.js
@@ -1,28 +1,32 @@
 ﻿
     $(document).ready(function () {
         var entrypush;
-        entrypush = $.connection.showAllUsers;
 
+  
+        entrypush = $.connection.showAllUsers;
+        
 
         entrypush.client.updateUserList = function(list){
-            
-            
+    
 
-                //$(".UserListz").append(list)
-                alert("Innan loop");
-            console.log("innan loopen");
-            for (var i = 0; i < list.Count; i++) {
-                $('#UserListz').append("<li>wigge</li>");
+                
+                
+             
+                
+            for (var i = 0; i <= list.length; i++) {
+
+                
+                console.log("före utskirft");
+                
+                
+                
+                
+                $('#UserListz').append("<div><p class='col-md-6';>" + list[i] + "</p><button class='col-md-6';>Lägg Till Vän</button></div>");
+
+
                 console.log("i loopen");
             }
-                
-
-
-            
-            
-            
         }
-
         //connect to hub
         $.connection.hub.start().done(function () {
             entrypush.server.send();
